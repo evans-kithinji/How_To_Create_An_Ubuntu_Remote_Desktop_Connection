f3ff70fcb645b88065a2d1456f64d28d1ca1442c
evans_kithinji <evanskithinji551@gmail.com>
2023-05-28 00:28:30 +0300

    first commit

---
+++ new.sh
@@ -0,0 +1,11 @@
+# A series of commands that are commonly used in Ubuntu or Debian-based Linux systems.
+# The series installs xrdp on Ubuntu
+sudo apt-get update
+sudo apt install xrdp
+sudo systemctl enable xrdp
+sudo apt-get dist-upgrade
+sudo add-apt-repository ppa:gnome3-team/gnome3
+sudo apt update
+sudo apt-get install gnome-shell ubuntu-gnome-desktop
+sudo apt-get update
+sudo passwd ubuntu
